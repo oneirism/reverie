8f3a9d4ee52e1d8b68f072daeeb54bddfc996ed1
Brendan Devenney <brendan@devenney.io>
2017-08-30 23:46:50 +0100

    Convert descriptions to markdown

--- reverie/settings.py
+++ reverie/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'markdown_deux',
+    'pagedown',
 ]
 
 # Use nose to run all tests
