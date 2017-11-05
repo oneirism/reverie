6da6d4263c2146f8544733973181e465ec398d7e
Brendan Devenney <brendan@devenney.io>
2017-11-05 20:33:03 +0000

    Exclude debug URL from testing

--- reverie/urls.py
+++ reverie/urls.py
@@ -37,4 +37,4 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # pragma: no cover
