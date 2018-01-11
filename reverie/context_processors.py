36935a2669361d862c7ffc908479e3c2305ae3c9
Brendan Devenney <brendan@devenney.io>
2018-01-11 19:09:59 +0000

    Address pylint issues

--- reverie/context_processors.py
+++ reverie/context_processors.py
@@ -3,6 +3,7 @@ from django.http import HttpRequest
 
 # Google Analytics Context
 def google_analytics(request: HttpRequest) -> dict:
+    #pylint: disable=unused-argument
     return {
         'GOOGLE_ANALYTICS_KEY': getattr(settings, 'GOOGLE_ANALYTICS_KEY', None),
     }
