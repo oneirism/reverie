a2f9b534bc7bc5aaf68b0b5e47e1282818e2d017
Brendan Devenney <brendan.devenney@cloudreach.com>
2018-04-18 17:49:27 +0100

    Scorched earth

--- reverie/tests/test_wsgi.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-
-from reverie import wsgi
-
-
-class WSGITest(TestCase):
-    def test_create_application(self):
-        application = wsgi.application
-        self.assertIsNotNone(application)
--- reverie/urls.py
+++ reverie/urls.py
@@ -1,40 +1,21 @@
 """reverie URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
-from django.conf.urls import include, url
-from django.conf.urls.static import static
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
-from django.urls import reverse_lazy
-
-from registration.backends.default.views import RegistrationView
-from registration.forms import RegistrationFormUniqueEmail
+from django.urls import path
 
 urlpatterns = [
-    url(r'^', include('campaign.urls', namespace='campaign')),
-    url(r'^accounts/register/$', RegistrationView.as_view(form_class=RegistrationFormUniqueEmail),
-        name='registration_register'),
-    url(r'^accounts/password/reset/$',
-        auth_views.password_reset, {
-            'post_reset_redirect': reverse_lazy('auth_password_reset_done'),
-            'html_email_template_name': 'registration/password_reset_email.html'
-        }, name='auth_password_reset'),
-    url(r'^accounts/', include('registration.backends.default.urls')),
-    url(r'^admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # pragma: no cover
